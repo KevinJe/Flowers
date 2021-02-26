a8fc8d20e371b2310ef32d4fc5bd482e540a8b85
test <test@gmail.com>
2021-02-26 15:21:37 +0800

    some changes

---
+++ app/src/main/java/com/example/flowers/adapters/GalleryAdapter.kt
@@ -0,0 +1,62 @@
+package com.example.flowers.adapters
+
+import android.content.Intent
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.flowers.data.UnsplashPhoto
+import com.example.flowers.databinding.ListItemPhotoBinding
+
+/**
+ * Adapter for the [RecyclerView] in [GalleryFragment].
+ */
+class GalleryAdapter : PagingDataAdapter<UnsplashPhoto, GalleryAdapter.GalleryViewHolder>(GalleryDiffCallback()) {
+
+
+    class GalleryViewHolder(
+        private val binding: ListItemPhotoBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        init {
+            binding.setClickListener { view ->
+                binding.photo?.let { photo ->
+                    val uri = Uri.parse(photo.user.attributionUrl)
+                    val intent = Intent(Intent.ACTION_VIEW, uri)
+                    view.context.startActivity(intent)
+                }
+            }
+        }
+
+        fun bind(item: UnsplashPhoto) {
+            binding.apply {
+                photo = item
+                executePendingBindings()
+            }
+        }
+    }
+
+    override fun onBindViewHolder(holder: GalleryViewHolder, position: Int) {
+        val photo = getItem(position)
+        if (photo != null) {
+            holder.bind(photo)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GalleryViewHolder {
+        return GalleryViewHolder(ListItemPhotoBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
+        ))
+    }
+}
+
+private class GalleryDiffCallback : DiffUtil.ItemCallback<UnsplashPhoto>() {
+    override fun areItemsTheSame(oldItem: UnsplashPhoto, newItem: UnsplashPhoto): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: UnsplashPhoto, newItem: UnsplashPhoto): Boolean {
+        return oldItem == newItem
+    }
+}
---
+++ app/src/main/java/com/example/flowers/api/UnsplashService.kt
@@ -0,0 +1,47 @@
+package com.example.flowers.api
+
+import com.example.flowers.BuildConfig
+import com.example.flowers.data.UnsplashSearchResponse
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+/**
+ * Used to connect to the Unsplash API to fetch photos
+ */
+interface UnsplashService {
+
+    @GET("search/photos")
+    suspend fun searchPhotos(
+        @Query("query") query: String,
+        @Query("page") page: Int,
+        @Query("per_page") perPage: Int,
+        @Query("client_id") clientId: String = BuildConfig.UNSPLASH_ACCESS_KEY
+    ): UnsplashSearchResponse
+
+    companion object {
+        private const val BASE_URL = "https://api.unsplash.com/"
+
+        fun create(): UnsplashService {
+            val logger =
+                HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BASIC }
+
+            val client = OkHttpClient.Builder()
+                .addNetworkInterceptor(logger)
+                .build()
+
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(UnsplashService::class.java)
+        }
+
+    }
+
+
+}
